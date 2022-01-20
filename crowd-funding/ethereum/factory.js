9af23267c45c6c07ba06a311fafe3e7b9da3750a
Salmaan Khan <salmaankhan@Salmaans-MacBook-Air.local>
2022-01-20 22:27:10 +0530

    web3 course stephen grider complete

--- crowd-funding/ethereum/factory.js
+++
@@ -1,7 +0,0 @@
-import web3 from "./web3";
-import CampaignFactory from "./build/CampaignFactory.json"
-
-const address = "0xa5C78383ad4C7fd16e9B407bB8F19E45609623Bb"
-const instance = new web3.eth.Contract(JSON.parse(CampaignFactory.interface), address )
-
-export default instance;
